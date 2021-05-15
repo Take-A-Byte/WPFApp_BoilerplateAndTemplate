43c67209cd3c3a201706a4e9ae0ad6dd0e084e40
Shantanu.Methikar <smethikar18@gmail.com>
2021-05-16 00:06:41 +0530

    initial code added for new project in WPF with MVVM framework

---
+++ WpfApp1.Translations/TranslationManager.cs
@@ -0,0 +1,16 @@
+﻿namespace WpfApp1.Translations
+{
+    using System;
+
+    public class TranslationManager
+    {
+        #region Public Methods
+
+        public string GetTranslation(string translationId)
+        {
+            return String.Empty;
+        }
+
+        #endregion Public Methods
+    }
+}
---
+++ WpfApp1/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace WpfApp1
+{
+    using System.Windows;
+    using WpfApp1.ViewModels;
+    using WpfApp1.Views;
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        #region Protected Methods
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            MainViewModel mainVM = new MainViewModel();
+            MainWindow = new MainWindow() { DataContext = mainVM };
+            MainWindow.Show();
+        }
+
+        #endregion Protected Methods
+    }
+}
